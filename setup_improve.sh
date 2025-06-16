5253ed7396392bd0d6c98006c00f0b33ba3c4ed4
nkoussa <156325369+nkoussa@users.noreply.github.com>
2025-06-16 16:20:40 -0400

    Update setup_improve.sh

--- setup_improve.sh
+++ setup_improve.sh
@@ -35,7 +35,7 @@ export IMPROVE_DATA_DIR="./$data_dir/"
 # Clone IMPROVE lib (if needed)
 pushd ../
 improve_lib_path=$PWD/IMPROVE
-improve_branch="v0.1.0-2024-09-27"
+improve_branch="v0.1.0"
 if [ -d $improve_lib_path ]; then
   echo "IMPROVE repo exists in ${improve_lib_path}"
 else
