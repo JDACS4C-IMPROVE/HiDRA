b1e84bdef93c2ec72fb6ed72bd6f88a1c4f7301a
Henry Raeder <ac.hraeder@hcdgx2.cels.anl.gov>
2025-12-09 20:59:28 -0600

    adding TCGA files

---
+++ HiDRA_infer_tcga.py
@@ -0,0 +1,74 @@
+import sys
+from pathlib import Path
+from typing import Dict
+import pandas as pd
+import json
+from hidra_utils import *
+
+# Import improvelib
+from improvelib.applications.drug_response_prediction.config import DRPInferConfig
+from improvelib.utils import str2bool
+import improvelib.utils as frm
+from model_params_def import infer_params
+
+filepath = Path(__file__).resolve().parent
+
+
+def run(params):
+    test_data_fname = frm.build_ml_data_file_name(data_format=params["data_format"], stage="tcga_test")
+
+    dir = Path(params["input_data_dir"])
+    expr = pd.read_csv(dir / 'cancer_ge_kegg.csv', index_col=0)
+    GeneSet_Dic = json.load(open(dir / 'geneset.json', 'r'))
+    drugs = pd.read_csv(dir / 'drug_ecfp4_nbits512.csv', index_col=0)
+    auc_test = pd.read_csv(dir / 'tcga_test_y_data.csv', index_col=0)
+    auc_test = auc_test[auc_test['improve_chem_id'].isin(drugs.index)]
+
+    test_label = auc_test[params['y_col_name']]
+    test_input = parse_data(auc_test, expr, GeneSet_Dic, drugs)
+
+    modelpath = frm.build_model_path(
+        model_file_name=params["model_file_name"],
+        model_file_format=params["model_file_format"],
+        model_dir=params["input_model_dir"])
+
+    model = load_model(str(modelpath))
+
+    test_label = test_label.to_numpy().flatten()
+    test_pred = model.predict(test_input).flatten()
+
+    frm.store_predictions_df(
+        y_true=test_label, y_pred=test_pred, stage="tcga_test",
+        y_col_name=params["y_col_name"],
+        output_dir=params["output_dir"],
+        input_dir=params["input_data_dir"]
+    )
+
+    print(params["calc_infer_scores"])
+
+    if params["calc_infer_scores"]:
+        test_scores = frm.compute_performance_scores(
+            y_true=test_label,
+            y_pred=test_pred,
+            stage="tcga_test",
+            metric_type=params["metric_type"],
+            output_dir=params["output_dir"]
+        )
+
+    return True
+
+
+def main(args):
+    additional_definitions = infer_params
+    cfg = DRPInferConfig()
+    params = cfg.initialize_parameters(
+        pathToModelDir=filepath,
+        default_config="hidra_params_tcga.txt",
+        additional_definitions=additional_definitions
+    )
+    status = run(params)
+    print("\nFinished inference with HiDRA.")
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
---
+++ HiDRA_preprocess_tcga.py
@@ -0,0 +1,111 @@
+import numpy as np
+import pandas as pd
+import sys
+import json
+from scipy.stats import zscore
+from pathlib import Path
+from typing import Dict
+
+# Core improvelib imports
+from improvelib.applications.drug_response_prediction.config import DRPPreprocessConfig
+from improvelib.utils import str2bool
+import improvelib.utils as frm
+
+# Application-specific (DRP) imports
+import improvelib.applications.drug_response_prediction.drug_utils as drugs_utils
+import improvelib.applications.drug_response_prediction.omics_utils as omics_utils
+import improvelib.applications.drug_response_prediction.drp_utils as drp
+
+# Model-specifc imports
+from model_params_def import preprocess_params
+from hidra_utils import *
+
+
+filepath = Path(__file__).resolve().parent
+
+
+def run(params: Dict):
+    """ Run data preprocessing.
+
+    Args:
+        params (dict): dict of IMPROVE parameters and parsed values.
+
+    Returns:
+        str: directory name that was used to save the preprocessed (generated)
+            ML data files.
+    """
+
+    print("\nLoading omics data...")
+    omics_obj = omics_utils.OmicsLoader(params)
+    ge = omics_obj.dfs['cancer_gene_expression.tsv']
+
+    genes_fpath = params["input_supp_data_dir"] + '/geneset.gmt'
+    ge, GeneSet_Dic = gene_selection(ge, genes_fpath, canc_col_name=params["canc_col_name"])
+
+    json.dump(GeneSet_Dic, open(params['output_dir'] + '/geneset.json', 'w'))
+
+    # Check that z-score is on the correct axis
+    numeric_cols = ge.select_dtypes(include=[np.number]).columns
+    ge[numeric_cols] = ge[numeric_cols].apply(zscore, axis=1)
+
+    print("\nLoading drugs data...")
+    drugs_obj = drugs_utils.DrugsLoader(params)
+    mf = drugs_obj.dfs['drug_ecfp4_nbits512.tsv']
+    mf = mf.reset_index()
+
+    ge.to_csv(params["output_dir"] + '/cancer_ge_kegg.csv', index=False)
+    mf.to_csv(params["output_dir"] + '/drug_ecfp4_nbits512.csv', index=False)
+
+    stages = {#"train": params["train_split_file"],
+              #"val": params["val_split_file"],
+              "tcga_test": params["test_split_file"]}
+
+    for stage, split_file in stages.items():
+        rsp = drp.DrugResponseLoader(params,
+                                     split_file=split_file,
+                                     verbose=False).dfs["response_tcga.tsv"]
+
+#        rsp = rsp.merge(ge[params["canc_col_name"]], on=params["canc_col_name"], how="inner")
+#        rsp = rsp.merge(mf[params["drug_col_name"]], on=params["drug_col_name"], how="inner")
+#        ge_sub = ge[ge[params["canc_col_name"]].isin(rsp[params["canc_col_name"]])].reset_index(drop=True)
+#        mf_sub = mf[mf[params["drug_col_name"]].isin(rsp[params["drug_col_name"]])].reset_index(drop=True)
+
+        data_fname = frm.build_ml_data_file_name(data_format=params["data_format"], stage=stage)
+
+#        print("Merge data")
+#        data = rsp.merge(ge_sc, on=params["canc_col_name"], how="inner")
+#        data = data.merge(md_sc, on=params["drug_col_name"], how="inner")
+#        data = data.sample(frac=1.0).reset_index(drop=True) # shuffle
+
+        print("Save data")
+#        data = data.drop(columns=["study"]) # to_parquet() throws error since "study" contain mixed values
+#        data.to_parquet(Path(params["output_dir"]) / data_fname) # saves ML data file to parquet
+
+        # Prepare the y dataframe for the current stage
+#        fea_list = ["ge", "mordred"]
+#        fea_cols = [c for c in data.columns if (c.split(fea_sep)[0]) in fea_list]
+#        meta_cols = [c for c in data.columns if (c.split(fea_sep)[0]) not in fea_list]
+#        ydf = data[meta_cols]
+        ydf = rsp
+        ydf = ydf[ydf['improve_chem_id'].isin(mf['improve_chem_id'])]
+
+        # [Req] Save y dataframe for the current stage
+        frm.save_stage_ydf(ydf, stage, params["output_dir"])
+
+    return params["output_dir"]
+
+
+def main(args):
+    additional_definitions = preprocess_params
+    cfg = DRPPreprocessConfig()
+    params = cfg.initialize_parameters(
+        pathToModelDir=filepath,
+        default_config="hidra_params_tcga.txt",
+        additional_definitions=additional_definitions,
+    )
+    ml_data_outdir = run(params)
+    print("\nFinished HiDRA pre-processing.")
+
+
+if __name__=="__main__":
+    main(sys.argv[1:])
